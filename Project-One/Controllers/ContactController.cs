723f7434b9789813b56c51371ad7ed1e39786b5e
J.Kisojan <45009613+kisojan@users.noreply.github.com>
2023-10-06 12:28:43 +0530

    Add files via upload

---
+++ Project-One/Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactForm.Models;
+
+ namespace ContactForm.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            Contact contactUs = new Contact();
+            return View(contactUs);
+        }
+        [HttpPost]
+        public ActionResult Index(Contact contactUs)
+        {
+            using (ContactDBEntities db = new ContactDBEntities())
+            {
+
+                if (db.Contacts.Any(x => x.Email == contactUs.Email))
+                {
+                    ViewBag.DuplicateMessage = "EmailID Already Exists";
+                    return View("Index", contactUs);
+                }
+                else
+                {
+                    db.Contacts.Add(contactUs);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.SuccessMessage = "Saved Successfully";
+                return View("Index", new Contact());
+
+            }
+        }
+    }
+}
